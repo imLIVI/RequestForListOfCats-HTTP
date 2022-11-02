099c3ea5ca6cf2f66b9eb626a21cbe77ba5a3689
imLIVI <lesly120898@gmail.com>
2022-11-02 22:42:28 +0300

    Implement a program to request for a list of facts about cats

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URI =
+            "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        //создание объекта запроса
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        //отправка запроса
+        CloseableHttpResponse response = httpClient.execute(request);
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        //преобразование json в java
+        List<Info> info = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Info>>() {
+        });
+        info.stream()
+                .filter(in -> in.getUpvotes() != null && Integer.valueOf(in.getUpvotes()) > 0)
+                .forEach(System.out::println);
+    }
+}
